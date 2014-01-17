4e02a3b45f7f445ad0911656ca086f980bad0412
mewmew <rnd0x00@gmail.com>
2014-01-17 06:59:02 +0100

    audio: Add skeleton structure of the audio API.

---
+++ audio/audio.go
@@ -0,0 +1,63 @@
+// Package audio provides support for audio playback.
+//
+// NOTE: This is a work in progrss. It is in the API design phase and no
+// functionality has yet been implemented.
+package audio
+
+import (
+	"io"
+	"time"
+)
+
+// A Stream is a sequence of audio samples.
+type Stream struct {
+}
+
+// New returns a new audio stream which reads and decodes its samples from the
+// provided io.Reader.
+func New(r io.Reader) (s *Stream, err error) {
+	panic("audio.New: not yet implemented.")
+}
+
+// Open returns a new audio stream which reads and decodes its samples from the
+// provided file.
+//
+// Note: The Close method should be called when done using the audio stream.
+func Open(filePath string) (s *Stream, err error) {
+	panic("audio.Open: not yet implemented.")
+}
+
+// TODO(u): document if Close is required to be called for client of both New
+// and Open.
+
+// Close closes the audio stream.
+func (s *Stream) Close() {
+	panic("Stream.Close: not yet implemented.")
+}
+
+// Play starts to play the audio stream from the current audio position.
+func (s *Stream) Play() {
+	panic("Stream.Play: not yet implemented.")
+}
+
+// Pause pauses the playback of the audio stream. The current audio position is
+// stored.
+func (s *Stream) Pause() {
+	panic("Stream.Pause: not yet implemented.")
+}
+
+// Stop stops the playback of the audio stream. The current audio position is
+// set to 0.
+func (s *Stream) Stop() {
+	panic("Stream.Stop: not yet implemented.")
+}
+
+// Seek seeks to the provided audio position.
+func (s *Stream) Seek(pos time.Duration) (err error) {
+	panic("Stream.Seek: not yet implemented.")
+}
+
+// Pos returns the current audio position of the audio stream.
+func Pos() (pos time.Duration) {
+	panic("Stream.Pos: not yet implemented.")
+}
